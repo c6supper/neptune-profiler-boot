8300b23a3ffea7bad9aae8cc6f9b85e6bdb90256
c6supper@hotmail.com <c6supper@hotmail.com>
2023-12-26 00:27:53 +0800

    update

---
+++ inc/trace/event_factory.h
@@ -0,0 +1,27 @@
+#ifndef EVENT_FACTORY_H_
+#define EVENT_FACTORY_H_
+
+#include <map>
+#include <memory>
+#include <mutex>
+#include <vector>
+
+#include "trace/trace_type.h"
+
+namespace coding_nerd::boot_perf {
+class EventFactory {
+  using type = EventFactory;
+
+ public:
+  EventFactory() = default;
+  virtual ~EventFactory() = default;
+
+  static std::shared_ptr<EventFactory> get();
+
+ private:
+  std::mutex mapMutex_;
+  std::map<uint32_t, std::vector<std::shared_ptr<traceevent>>> combinedEvent_;
+};
+}  // namespace coding_nerd::boot_perf
+
+#endif  // EVENT_FACTORY_H_
