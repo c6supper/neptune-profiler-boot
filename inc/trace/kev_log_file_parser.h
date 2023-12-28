c7650654d99bd6535f6f5b6db3ced01082cd8098
c6supper@hotmail.com <c6supper@hotmail.com>
2023-12-28 12:12:16 +0000

    update

--- inc/trace/kev_log_file_parser.h
+++ inc/trace/kev_log_file_parser.h
@@ -20,7 +20,9 @@ class KeyLogFileParser : public TraceParser<std::ifstream, Out> {
  public:
   KeyLogFileParser()
       : TraceParser<std::ifstream, Out>(),
-        trace_clock_(std::make_shared<TraceClock>()){};
+        trace_clock_(std::make_shared<TraceClock>()),
+        event_json_factory_(
+            EventFactory<nlohmann::json, traceevent>::Instance()){};
   ~KeyLogFileParser() override = default;
   KeyLogFileParser(KeyLogFileParser& other) = delete;
   KeyLogFileParser& operator=(KeyLogFileParser& other) = delete;
@@ -45,6 +47,7 @@ class KeyLogFileParser : public TraceParser<std::ifstream, Out> {
  private:
   std::unique_ptr<TraceHeader> trace_header_;
   std::shared_ptr<TraceClock> trace_clock_;
+  std::unique_ptr<EventFactory<nlohmann::json, traceevent>> event_json_factory_;
 };
 
 }  // namespace coding_nerd::boot_perf
